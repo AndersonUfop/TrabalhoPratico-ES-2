2283b8d7273f18b309afa27d5b4cf8e2971da59a
AndersonUfop <andersonfferreira13@hotmail.com>
2020-09-09 13:53:59 -0300

    Adicionando pet

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,35 @@
+const crypto = require('crypto');
 const express = require('express');
+const knex = require('../database/connection');
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+routes.get('/pets', (request, response) => {
+  return response.json({ message: 'Pets' });
 
+});
+
+routes.post('/create', async(request, response) => {
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  const {
+    image,
+    type,
+    sex,
+    breed,
+    coat
+  } = request.body;
+
+  await knex('pets').insert({
+    id,
+    image,
+    type,
+    sex,
+    breed,
+    coat
+  });
+
+  return response.json({ success: true })
 });
 
 module.exports = routes;
