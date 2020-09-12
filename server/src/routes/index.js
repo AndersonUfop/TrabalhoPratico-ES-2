9aed0203d6703876e248e4e84b0c98b00566e112
AndersonUfop <andersonfferreira13@hotmail.com>
2020-09-12 18:42:30 -0300

    Criando o CRUD

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,11 +4,15 @@ const knex = require('../database/connection');
 
 const routes = express.Router();
 
-routes.get('/pets', (request, response) => {
-  return response.json({ message: 'Pets' });
+// Listar pets
+routes.get('/pets', async(request, response) => {
+  const pets = await knex('pets').select('*');
+
+  return response.json(pets);
 
 });
 
+// Cadastrar pets
 routes.post('/create', async(request, response) => {
   const id = crypto.randomBytes(4).toString('HEX');
 
@@ -31,6 +35,41 @@ routes.post('/create', async(request, response) => {
 
   return response.json({ success: true })
 });
+
+// Excluir pet
+routes.delete('/delete/:id', async(request, response) => {
+  const { id } = request.params;
+
+  await knex('pets').where('id', id).delete();
+
+  return response.status(204).send();
+})
+
+// Editar pet
+
+routes.put('/update/:id', async(request, response) => {
+  const {
+    image,
+    type,
+    sex,
+    breed,
+    coat
+  } = request.body;
+
+  const { id } = request.params;
+
+  await knex('pets')
+    .update({
+      id,
+      image,
+      type,
+      sex,
+      breed,
+      coat
+     }).where({ id });
+
+    return response.send();
+})
 
 module.exports = routes;
 
