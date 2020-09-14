c4e2e3149f8123a599423caaac0865869db4f320
AndersonUfop <andersonfferreira13@hotmail.com>
2020-09-13 22:23:23 -0300

    Finalizando o CRUD

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,75 +1,20 @@
-const crypto = require('crypto');
 const express = require('express');
-const knex = require('../database/connection');
+
+const PetsController = require('../controllers/PetsController');
 
 const routes = express.Router();
 
-// Listar pets
-routes.get('/pets', async(request, response) => {
-  const pets = await knex('pets').select('*');
-
-  return response.json(pets);
-
-});
-
 // Cadastrar pets
-routes.post('/create', async(request, response) => {
-  const id = crypto.randomBytes(4).toString('HEX');
+routes.post('/create', PetsController.create);
 
-  const {
-    image,
-    type,
-    sex,
-    breed,
-    coat
-  } = request.body;
+// Listar pets
+routes.get('/pets', PetsController.index);
 
-  await knex('pets').insert({
-    id,
-    image,
-    type,
-    sex,
-    breed,
-    coat
-  });
-
-  return response.json({ success: true })
-});
+// Editar pets
+routes.put('/update/:id', PetsController.update);
 
 // Excluir pet
-routes.delete('/delete/:id', async(request, response) => {
-  const { id } = request.params;
-
-  await knex('pets').where('id', id).delete();
-
-  return response.status(204).send();
-})
-
-// Editar pet
-
-routes.put('/update/:id', async(request, response) => {
-  const {
-    image,
-    type,
-    sex,
-    breed,
-    coat
-  } = request.body;
-
-  const { id } = request.params;
-
-  await knex('pets')
-    .update({
-      id,
-      image,
-      type,
-      sex,
-      breed,
-      coat
-     }).where({ id });
-
-    return response.send();
-})
+routes.delete('/delete/:id', PetsController.delete);
 
 module.exports = routes;
 
